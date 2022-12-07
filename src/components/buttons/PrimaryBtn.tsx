90627821f19f143790c6c5a71a932f3525be8574
clayton-schneider <schneiderc2016@gmail.com>
2022-12-07 17:04:46 -0500

    footer desktop done

---
+++ src/components/buttons/PrimaryBtn.tsx
@@ -0,0 +1,22 @@
+import type { ReactNode } from "react";
+
+interface Props {
+  children: ReactNode;
+  to?: string;
+  type?: "submit" | "reset" | "button";
+  className?: string;
+}
+const PrimaryBtn = ({ children, to, className, type = "button" }: Props) => (
+  <a
+    type={type}
+    href={to}
+    className={
+      className +
+      " bg-black inline-block px-16 py-3 rounded-full font-bebas text-2xl text-white cursor-pointer"
+    }
+  >
+    {children}
+  </a>
+);
+
+export default PrimaryBtn;
