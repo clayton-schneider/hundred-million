6d07de631324115202d927722660ba272d5a5d49
clayton-schneider <schneiderc2016@gmail.com>
2022-12-08 13:45:13 -0500

    image section start

--- src/components/buttons/PrimaryBtn.tsx
+++ src/components/buttons/PrimaryBtn.tsx
@@ -6,14 +6,23 @@ interface Props {
   to?: string;
   type?: "submit" | "reset" | "button";
   className?: string;
+  // Customize to theme
+  color?: "primary" | "black";
 }
-const PrimaryBtn = ({ children, to, className, type = "button" }: Props) => (
+const PrimaryBtn = ({
+  children,
+  to,
+  className,
+  type = "button",
+  color = "black",
+}: Props) => (
   <a
     type={type}
     href={to}
     className={
       className +
-      " bg-black inline-block px-16 py-3 rounded-full font-bebas text-2xl text-white cursor-pointer"
+      ` bg-${color}` +
+      " inline-block px-16 py-3 rounded-full font-bebas text-2xl text-white cursor-pointer"
     }
   >
     {children}
